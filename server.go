e60fe68f454e987c8709b8c9eec88e2478fe8adb
Paul Caffrey <58259624+pcaff321@users.noreply.github.com>
2021-09-06 20:53:07 +0100

    Add files via upload

--- server.go
+++ server.go
@@ -19,6 +19,74 @@ func findNoteIndex(note string) int{
 	return -1
 }
 
+func diminishedChord(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	dirs := strings.Split(r.URL.String(), "/")
+
+
+
+	if  ((len(dirs) >= 3) && (len(dirs[2]) > 0)){
+	note := strings.ToUpper(string(dirs[2][0]))
+	if len(dirs[2]) > 1 {
+	note += "#"
+	}
+	noteIndex := findNoteIndex(note)
+	if noteIndex == -1 {
+	fmt.Fprintf(w, "ERROR: Please enter an acceptable note")
+	return
+	}
+	fmt.Fprintf(w, "Note Chosen As Root: " + note + "\n")
+	chordFormation := note + " " +  notes[(noteIndex + 3) % len(notes)] + " " + notes[(noteIndex + 6) % len(notes)]
+	if (len(dirs) > 3) && (strings.ToUpper(dirs[3]) == "SEVENTH") { 
+		chordFormation += " " + notes[(noteIndex + 9) % len(notes)]
+		fmt.Fprintf(w, "Diminished Seventh Chord: " + chordFormation)
+		return
+	}
+	fmt.Fprintf(w, "Diminished Chord: " + chordFormation)
+	return
+	}
+
+	fmt.Fprintf(w, "ERROR: Please enter an acceptable note")
+	return
+}
+
+func augmentedChord(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	dirs := strings.Split(r.URL.String(), "/")
+
+
+
+	if  ((len(dirs) >= 3) && (len(dirs[2]) > 0)){
+	note := strings.ToUpper(string(dirs[2][0]))
+	if len(dirs[2]) > 1 {
+	note += "#"
+	}
+	noteIndex := findNoteIndex(note)
+	if noteIndex == -1 {
+	fmt.Fprintf(w, "ERROR: Please enter an acceptable note")
+	return
+	}
+	fmt.Fprintf(w, "Note Chosen As Root: " + note + "\n")
+	chordFormation := note + " " +  notes[(noteIndex + 4) % len(notes)] + " " + notes[(noteIndex + 8) % len(notes)]
+	if (len(dirs) > 3) && (strings.ToUpper(dirs[3]) == "SEVENTH") { 
+		chordFormation += " " + notes[(noteIndex + 10) % len(notes)]
+		fmt.Fprintf(w, "Augmented Seventh Chord: " + chordFormation)
+		return
+	}
+	fmt.Fprintf(w, "Augmented Chord: " + chordFormation)
+	return
+	}
+
+	fmt.Fprintf(w, "ERROR: Please enter an acceptable note")
+	return
+}
+
 func minorChord(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Add("content-type", "application/json")
@@ -94,6 +162,8 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Supported Chords: Major, Minor, Major Sevenths, Minor Sevenths \n")
 	fmt.Fprintf(w, "Hint: Go to /minor/A to see the notes in Am! \n")
 	fmt.Fprintf(w, "Hint: Go to /major/C to see the notes in C Major! \n")
+	fmt.Fprintf(w, "Hint: Go to /augmented/F to see the notes in F Augmented! \n")
+	fmt.Fprintf(w, "Hint: Go to /diminished/B to see the notes in B diminished! \n")
 	fmt.Fprintf(w, "Hint: To make it a seventh chord, add /seventh/ at the end of the URL \n")
 	return
 }
@@ -104,6 +174,8 @@ func main() {
 	http.HandleFunc("/", mainPage)
 	http.HandleFunc("/minor/",  minorChord)
 	http.HandleFunc("/major/",  majorChord)
+	http.HandleFunc("/diminished/",  diminishedChord)
+	http.HandleFunc("/augmented/",  augmentedChord)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
